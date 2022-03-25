d3f41e2e5ffc680128ca9ebd6527a4acc0462e07
KafipHero <alkafi28@gmail.com>
2022-03-26 02:03:33 +0600

    products added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Cart = () => {
     return (
-        <div className='col-4'>
-<h1>This is cart</h1>
+        <div className='col-4 mt-5'>
+            <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Selected Items</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <button className="btn btn-outline-primary">Go somewhere</button>
+                    </div>
+            </div>
         </div>
     );
 };
