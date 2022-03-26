889fd21600528cdf632374e15c3b676feced23f3
KafipHero <alkafi28@gmail.com>
2022-03-26 11:20:53 +0600

    cart added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = (props) => {
+    // const { thumbnail, name } = props.product;
     return (
         <div className='col-4 mt-5'>
-            <div className="card">
+            <div className="card bg-light">
                     <div className="card-body">
                         <h5 className="card-title">Selected Items</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
